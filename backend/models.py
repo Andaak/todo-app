abc3bf14dd8db251e8963ac35153f7ca4333b89a
André Åkerholm <andre_aakerholm@hotmail.com>
2023-04-10 17:14:47 +0200

    Added more values to api

--- backend/models.py
+++ backend/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 
 class Todo(models.Model):
+    oprettet = models.DateTimeField(auto_now_add=True)
     oppgavetekst = models.CharField(max_length=200)
+    frist = models.DateTimeField(null=True, blank=True)
     ferdig = models.BooleanField(default=False)
     
     def __str__(self):
-        return f'{self.oppgavetekst}'
+        return f'{self.oppgavetekst} - {self.oprettet}'
